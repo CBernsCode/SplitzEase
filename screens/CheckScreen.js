a63f9e7ac7e86b918ce93d4dc9ce784298909d8d
Kyle Demers <kyledemers@gmail.com>
2019-04-07 15:52:16 -0400

    Connect frontend and backend of Checks

--- screens/CheckScreen.js
+++ screens/CheckScreen.js
@@ -1,19 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, Card, FlatList, Modal, ScrollView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Constants } from 'expo';
 import Colors from '../constants/Colors';
-
-/*
-  Check Screen
-    [ ] Should be used for order history
-    [ ] Should be used to display unpaid orders
-*/
-
-const sampleFriend = {
-  added: Date.now(),
-  name: "Dave",
-  id: Math.random()
-}
 
 export default class CheckScreen extends React.Component {
   constructor(props) {
@@ -32,81 +20,40 @@ export default class CheckScreen extends React.Component {
     },
   };
 
-  state = {
-    modalVisible: false,
-  };
-
-  setModalVisible = (visible) => {
-    this.setState({ modalVisible: visible, });
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Body />  
-          {/*onPress={() => this.props.frndActions.addFriend('21312', sampleFriend)}*/}
-          <Button
-            color={Colors.button}
-            title="Add Friend"
-            onPress={() => this.setModalVisible(!this.state.modalVisible)}
-          />
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={this.state.modalVisible}
-            onRequestClose={() => this.setState({ modalVisible: false })}
-          >
-          <View style={styles.modalOuter}>
-            <View style={styles.modalInner}>
-              <TextInput
-                style={styles.modalMultilineInput}
-
-                autoCorrect={false}
-                enablesReturnKeyAutomatically={true}
-                label='Friends'
-                placeholder='friend@splitzease.com'
-                returnKeyType='send'
-              />
-              <View style={styles.modalButton}>
-                {/* Add Friend Button should actually add a friend in the future */}
-                <Button color={Colors.fabButton} title='Add Friend' onPress={() => this.setModalVisible(!this.state.modalVisible)} />
-                <Button color={Colors.fabButton} title='Cancel' onPress={() => this.setModalVisible(!this.state.modalVisible)} />
-              </View>
-            </View>
-          </View>
-        </Modal>
+        <Body {...this.props} />
       </View>
     );
   }
 }
 
-const check_data = [
-  {
-    key: '0000000001',
-    restaurant: 'Applebee\'s',
-    description: "1x BLT",
-
-    amount: '22.00',
-    tax: '2.00',
-    total: '24.00',
-  },
-  {
-    key: '0000000002',
-  },
-  {
-    key: '0000000003',
-  },
-];
-
 class Body extends React.PureComponent {
+  componentDidMount () {
+    const { account } = this.props;
+    !!account && !!account.user && this.props.chkActions.getChecks(account.user.uid);
+  }
+
   render() {
-    return (
+    // console.log(this.props.checks.arr)
+
+    if(this.props.checks.arr[0] != undefined) {
+      return (
         <FlatList
-            data={check_data}
-            renderItem={({item}) => <Check id={item.key} {...item}/>}
+            data={this.props.checks.arr}
+            keyExtractor={(item, index) => item.id.toString()}
+            renderItem={({item}) => <Check id={item.id} uid={this.props.account.user.uid} chkActions={this.props.chkActions} {...item}/>}
             style={styles.body}
         />
-    );
+      );
+    } else {
+      return (
+        <View style={styles.body}>
+          <Text style={styles.noChecks}>Loading...</Text>
+        </View>
+      );
+    }
   }
 }
 
@@ -117,21 +64,14 @@ class Check extends React.Component {
     this.state = {
       isPaid : false,
     };
-
-    // this.pay = this.pay.bind(this);
-    // only necessary to define "this" when using
-    //    pay(){...} (Function.prototype.bind)
-    //    instead of 
-    //    pay = () => {...} (arrow functions)
   }
 
   pay = () => {
     this.setState({
       isPaid : true,
     });
-
-    // accept payment amount
-    // accept payment type
+    // console.log(this.props);
+    this.props.chkActions.payCheck(this.props.uid, this.props.id, this.props.amount) // pay check...?
   }
 
   render() {
@@ -142,10 +82,8 @@ class Check extends React.Component {
                 <Text style={styles.checkHeader}>Check #{this.props.id || "0000000000"}</Text>
                 <Text style={styles.tabbedText}>Restaurant: {this.props.restaurant || "Some Restaurant"}</Text>
                 <Text style={styles.tabbedText}>Description: {this.props.description || "Some food that was ordered." }</Text>
-                <Text style={styles.tabbedText}>Amount Due: ${this.props.amount || "0.00"}</Text>
-                <Text style={styles.tabbedText}>Tax: ${this.props.tax || "0.00"}</Text>
-                <Text style={styles.tabbedText}>Total Due:</Text>
-                <Text style={styles.total}>${this.props.total || "0.00"}</Text>
+                <Text style={styles.tabbedText}>Amount Due:</Text>
+                <Text style={styles.total}>${this.props.price || "0.00"}</Text>
                 <View style={styles.button}><Button color={Colors.button} onPress={this.pay} title="Pay"></Button></View>
             </View>
         </View>
@@ -204,52 +142,9 @@ const styles = StyleSheet.create({
     /* shadowOpacity: 0.5, */
   },
 
-  modalButton: {
-    bottom: 20,
-    justifyContent: 'center',
-    position: 'absolute',
-    width: 300,
-  },
-
-  modalInner: {
-    backgroundColor: Colors.background,
-    borderRadius: 20,
-    height: 300,
-    padding: 20,
-    width: 300,
-  },
-
-  modalInput: {
-    backgroundColor: Colors.cardBackground,
-    borderRadius: 5,
-    color: Colors.text,
-    fontSize: 20,
-    height: 40,
-    marginBottom: 10,
-    paddingLeft: 20,
-  },
-
-  modalMultilineInput: {
-    backgroundColor: Colors.cardBackground,
-    borderRadius: 5,
-    color: Colors.text,
-    fontSize: 20,
-    height: 40,
-    marginBottom: 10,
-    paddingLeft: 20,
-  },
-
-  modalOuter: {
-    alignItems: 'center',
-    backgroundColor: Colors.transparentBackDrop,
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    padding: 20,
-  },
-
-  modalTitle: {
-    fontSize: 25,
+  noChecks: {
+    margin: 10,
+    textAlign: 'center',
   },
 
   statusBar: {
